4d1d891a5ea252c35b0c0318aeda55738d5173cb
IvanTSY <deadline.3012@gmail.com>
2019-02-28 17:33:32 +0700

    Test2
    Test2

--- JavaAppiumAutomation/src/MainClass.java
+++ JavaAppiumAutomation/src/MainClass.java
@@ -1,7 +1,17 @@
 
 public class MainClass {
-//Test 1
-   public int getLocalNumber(){
+
+    //Test 0.1
+
+    public int getLocalNumber(){
         return (14);
+    }
+
+    //Test 0.2
+
+    private int class_number = 20;
+
+    public int getClassNumber(){
+        return (this.class_number);
     }
 }
--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass{
 
+    //Test 0.1
     @Test
 
     public void testGetLocalNumber(){
@@ -12,5 +13,15 @@ public class MainClassTest extends MainClass{
         }else System.out.println("Test fail! getLocalNumber return number "+getLocalNumber()+". This number is smaller then 14.");
 
     }
+
+    //Test 0.2
+    @Test
+
+    public void testGetClassNumber(){
+        if (getClassNumber() > 45){
+            System.out.println("Test pass!");
+        }else System.out.println("Test fail! ClassNumber not larger number 45");
+    }
+
 
 }
